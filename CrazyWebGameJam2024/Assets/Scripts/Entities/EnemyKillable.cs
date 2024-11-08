a15419df752d07943829abd717f3e244f1a6f76f
danielconde001 <danielconde001@yahoo.com>
2024-11-08 23:29:30 +0800

    push maluoosh

--- CrazyWebGameJam2024/Assets/Scripts/Entities/EnemyKillable.cs
+++ CrazyWebGameJam2024/Assets/Scripts/Entities/EnemyKillable.cs
@@ -1,8 +1,21 @@
+using UnityEngine;
 public class EnemyKillable : Killable
 {
+    [SerializeField] private bool destroyAfterDeath = false;
+    [SerializeField] private float destroyAfterDeathTime = 2f;
+    
     public override void Death()
     {
         base.Death();
         GameManager.Instance().AddKillCount();
+        if (destroyAfterDeath)
+        {
+            Invoke("DestroyEnemy", destroyAfterDeathTime);
+        }
+    }
+
+    protected virtual void DestroyEnemy()
+    {
+        Destroy(this.gameObject);
     }
 }
